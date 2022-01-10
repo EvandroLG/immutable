91c118ac050df5cda0b17c82237bb31465cbec12
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2022-01-10 09:44:19 -0300

    add comments

--- src/immutable/init.lua
+++ src/immutable/init.lua
@@ -1,3 +1,6 @@
+-- makes a deep copy from a table
+-- @param state {table}
+-- @return {table}
 local function deep_copy(state)
   local output = {}
 
@@ -12,6 +15,10 @@ local function deep_copy(state)
   return output
 end
 
+-- takes two tables and compare them, returning true in case they are equal
+-- @param state {table}
+-- @param copy {table}
+-- @return {boolean}
 local function compare(state, copy)
   if type(state) ~= type(copy) then
     return false
@@ -24,6 +31,7 @@ local function compare(state, copy)
   local is_equal = true
 
   for k, v in pairs(copy) do
+    -- in case state and copy have the same value, keep data structure from state
     if compare(state[k], v) then
       copy[k] = state[k]
     else
@@ -31,20 +39,34 @@ local function compare(state, copy)
     end
   end
 
+  -- look for removed items from the original state
   for k in pairs(state) do
     if copy[k] == nil then
       is_equal = false
+      break
     end
   end
 
   return is_equal
 end
 
+-- immutable helper
+-- @param state {table}
+-- @param recipe {function}
+-- @return {table}
 local function immutable(state, recipe)
+  if type(state) ~= 'table' then
+    error('`state` should be a `table`')
+  end
+
+  if type(recipe) ~= 'function' then
+    error('`recipe` should be a `function`')
+  end
+
   local copy = deep_copy(state)
   recipe(copy)
 
-  if (compare(state, copy)) then
+  if compare(state, copy) then
     return state
   end
 
